d427e046e9ae15db2491791f95f2eaa6037b895b
Devve08 <mhamad.safa.08@gmail.com>
2021-06-14 13:42:04 +0300

    step 3

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const port = 3000;
 app.get('/', (req, res) =>{
     res.send('ok')
 })
+// display time when /time is invoked
+
+
+    let x = new Date()
+    let y = x.getHours()
+    let z = x.getMinutes()
+
+
+app.get('/time', (req, res) =>{
+    res.send(`{status:200, message: ${y}:${z}}`)
+})
+// display ok when /text invoked
+app.get('/test', (req, res) =>{
+    res.send(`{status:200, message:"ok"}`)
+})
 
 app.listen(port, () =>{
     console.log(`hello at http://localhost:${port}`)
